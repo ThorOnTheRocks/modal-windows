eab3be03911f155cfd1a88a17d1c6e5684fc363a
gianluca galota <gianluca.galota@gmail.com>
2021-06-17 12:40:26 +0100

    close modal added click and escape key event

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-// Selecting DOM elements
+'use strict';
 
+// Selecting DOM elements
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btns = document.querySelectorAll('.show-modal');
-const closeModal = document.querySelector('.close-modal');
-const body = document.querySelector('body');
-
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 
 // Open Modal Window
-const openModal = btns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  })
-});
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
 
-closeModal.addEventListener('click', () => {
+const closeModal = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
+}
+
+// Adding Event Listener
+btnsOpenModal.forEach((btn => {
+  btn.addEventListener('click', openModal)
 })
+);
 
+btnCloseModal.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
 
-
-
-
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
